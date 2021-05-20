46b2fee850756c57eea3e2a85e1cb16bf80f019b
Daiane Silva <daianemsilva09@gmail.com>
2021-05-20 13:01:27 -0300

    inicio

--- course-manager/src/app/app.component.ts
+++ course-manager/src/app/app.component.ts
@@ -6,7 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'course-managerrrrr';
+  title = 'course-manager';
 
-  name: string = 'Johnnnnnn';
+  name: string = 'Jon';
 }
